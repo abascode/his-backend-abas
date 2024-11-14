d569c3dc43d5dfb7336af8567b3c3e82b8b8f563
tiffany <tiffanylumengkewas@gmail.com>
2024-11-14 21:53:21 +0700

    migration revision

--- src/domains/master/entities/va_segment.py
+++ src/domains/master/entities/va_segment.py
@@ -15,8 +15,8 @@ class Segment(BaseModel):
     created_at: MappedColumn[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: MappedColumn[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     deletable: MappedColumn[int] = mapped_column(Integer, server_default=text("0"))
-    percentage: MappedColumn[int] = mapped_column(Integer, nullable=False)
-    month: MappedColumn[int] = mapped_column(Integer, nullable=False)
-    year: MappedColumn[int] = mapped_column(Integer, nullable=False)
+    percentage: MappedColumn[int] = mapped_column(Integer)
+    month: MappedColumn[int] = mapped_column(Integer)
+    year: MappedColumn[int] = mapped_column(Integer)
 
     category: MappedColumn[Category] = relationship(Category)
