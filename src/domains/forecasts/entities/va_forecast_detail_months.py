62df5bc144eddfac8d29864973199bbca6cc6c45
Devin Anugrah <devinanugrahp27@gmail.com>
2024-11-19 11:06:27 +0700

    init: init entities

---
+++ src/domains/forecasts/entities/va_forecast_detail_months.py
@@ -0,0 +1,5 @@
+
+from src.shared.entities.basemodel import BaseModel
+
+class ForecastDetailMonth(BaseModel):
+    pass
---
+++ src/domains/forecasts/entities/va_forecasts.py
@@ -0,0 +1,5 @@
+
+from src.shared.entities.basemodel import BaseModel
+
+class Forecast(BaseModel):
+    pass
