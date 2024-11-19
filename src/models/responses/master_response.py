a7cd38437a22ef924e7c317b025d2266f106896a
Ari Davis <aridavis6688@gmail.com>
2024-11-19 15:31:05 +0700

    stock pilot response

--- src/models/responses/master_response.py
+++ src/models/responses/master_response.py
@@ -2,3 +2,8 @@ class OrderConfigurationResponse:
     category_id: str
     forecast_percentage: int
     urgent_percentage: int
+
+
+class StockPilotResponse:
+    segment_id: str
+    percentage: int
