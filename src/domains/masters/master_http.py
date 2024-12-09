5d35419c1208a989d01c7a534cd8a2e463cd4b96
tiffany <tiffanylumengkewas@gmail.com>
2024-12-09 08:23:21 +0700

    fix:stock pilots and format code

--- src/domains/masters/master_http.py
+++ src/domains/masters/master_http.py
@@ -3,15 +3,22 @@ from starlette.requests import Request
 
 from src.domains.masters.master_interface import IMasterUseCase
 from src.domains.masters.master_usecase import MasterUseCase
-from src.models.requests.master_request import GetOrderConfigurationRequest, GetStockPilotRequest
-from src.models.responses.basic_response import TextValueResponse,ListResponse
-from src.models.responses.master_response import OrderConfigurationsResponse, StockPilotsResponse
+from src.models.requests.master_request import (
+    GetOrderConfigurationRequest,
+    GetStockPilotRequest,
+)
+from src.models.responses.basic_response import TextValueResponse, ListResponse
+from src.models.responses.master_response import (
+    OrderConfigurationsResponse,
+    StockPilotsResponse,
+)
 
 router = APIRouter(prefix="/api/master", tags=["Master"])
 
+
 @router.get(
     "/dealers/options",
-    response_model= ListResponse[TextValueResponse],
+    response_model=ListResponse[TextValueResponse],
     summary="Dealer Options",
     description="Dealer Options",
 )
@@ -19,13 +26,14 @@ def get_dealer_options(
     request: Request,
     search: str | None = "",
     dealer_uc: IMasterUseCase = Depends(MasterUseCase),
-)->ListResponse[TextValueResponse]:
-    dealers = dealer_uc.get_dealer_options(request,search)
+) -> ListResponse[TextValueResponse]:
+    dealers = dealer_uc.get_dealer_options(request, search)
     return ListResponse(data=dealers, message="Success Fetching Dealer Options")
+
 
 @router.get(
     "/models/options",
-    response_model= ListResponse[TextValueResponse],
+    response_model=ListResponse[TextValueResponse],
     summary="Model Options",
     description="Model Options",
 )
@@ -33,8 +41,8 @@ def get_model_options(
     request: Request,
     search: str | None = "",
     model_uc: IMasterUseCase = Depends(MasterUseCase),
-)->ListResponse[TextValueResponse]:
-    models = model_uc.get_model_options(request,search)
+) -> ListResponse[TextValueResponse]:
+    models = model_uc.get_model_options(request, search)
     return ListResponse(data=models, message="Success Fetching Model Options")
 
 
@@ -44,29 +52,31 @@ class OrderApprovalMatrixResponse:
 
 @router.get(
     "/order-configuration",
-    response_model= ListResponse[OrderConfigurationsResponse],
+    response_model=ListResponse[OrderConfigurationsResponse],
     summary="Order Configuration",
     description="Order Configuration",
 )
 def get_order_configuration(
     request: Request,
-    order_configuration_request:GetOrderConfigurationRequest=Depends(),
+    order_configuration_request: GetOrderConfigurationRequest = Depends(),
     order_config_uc: IMasterUseCase = Depends(MasterUseCase),
-)->ListResponse[OrderConfigurationsResponse]:
-    orders = order_config_uc.get_order_configuration(request,order_configuration_request)
+) -> ListResponse[OrderConfigurationsResponse]:
+    orders = order_config_uc.get_order_configuration(
+        request, order_configuration_request
+    )
     return ListResponse(data=orders, message="Success Fetching Orders Configuration")
+
 
 @router.get(
     "/stock-pilots",
-    response_model= ListResponse[StockPilotsResponse],
+    response_model=ListResponse[StockPilotsResponse],
     summary="Stock Pilot",
     description="Stock Pilot",
 )
 def get_stock_pilots(
     request: Request,
-    stock_pilots_request:GetStockPilotRequest=Depends(),
-    order_config_uc: IMasterUseCase = Depends(MasterUseCase),
-)->ListResponse[StockPilotsResponse]:
-    stock_pilots = order_config_uc.get_order_configuration(request,stock_pilots_request)
-    return ListResponse(data=stock_pilots, message="Success Fetching Orders Configuration")
-
+    stock_pilots_request: GetStockPilotRequest = Depends(),
+    stock_pilot_uc: IMasterUseCase = Depends(MasterUseCase),
+) -> ListResponse[StockPilotsResponse]:
+    stock_pilots = stock_pilot_uc.get_stock_pilots(request, stock_pilots_request)
+    return ListResponse(data=stock_pilots, message="Success Fetching Stock Pilot")
